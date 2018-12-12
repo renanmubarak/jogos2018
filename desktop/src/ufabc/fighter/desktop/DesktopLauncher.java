7ba5c4338e8fe89ff0faa0e48c7abce313e1c201
renanmubarak <40616593+renanmubarak@users.noreply.github.com>
2018-12-11 23:28:58 -0200

    Add files via upload

---
+++ desktop/src/ufabc/fighter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ufabc.fighter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ufabc.fighter.UFABCFighter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UFABCFighter(), config);
+	}
+}
